dcd25195fe920c272a720f1b86ef50d235870437
Anwesh43 <anweshthecool0@gmail.com>
2017-05-11 13:16:17 +0530

    adding heghtlighview

---
+++ highlistviewlist/src/main/java/com/anwesome/ui/highlistviewlist/HightLightView.java
@@ -0,0 +1,65 @@
+package com.anwesome.ui.highlistviewlist;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by anweshmishra on 11/05/17.
+ */
+public class HightLightView extends View{
+    private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private int w,h,time;
+    private float y = 0,initY;
+    private List<ImageContainer> imageContainers = new ArrayList<>();
+    public HightLightView(Context context) {
+        super(context);
+    }
+    public void onDraw(Canvas canvas) {
+        if(time == 0) {
+            w = canvas.getWidth();
+            h = canvas.getHeight();
+            initY = y;
+        }
+        time++;
+    }
+    public void update(float factor) {
+        y = (initY-h)*factor;
+        postInvalidate();
+    }
+    public void addBitmap(Bitmap bitmap) {
+        float y = this.y;
+        if(imageContainers.size() > 0) {
+            ImageContainer imageContainer = imageContainers.get(0);
+            y = imageContainer.y;
+        }
+        ImageContainer newImageContainer = new ImageContainer(bitmap,y+h);
+        imageContainers.add(newImageContainer);
+    }
+    public void stop() {
+        if(imageContainers.size() > 0) {
+            imageContainers.remove(0);
+            initY = y;
+        }
+
+    }
+    private class ImageContainer {
+        private Bitmap bitmap;
+        private float y = 0;
+        public ImageContainer(Bitmap bitmap,float y) {
+            this.bitmap = bitmap;
+            this.y = y;
+        }
+        public void draw(Canvas canvas) {
+            canvas.save();
+            canvas.translate(0,y);
+            canvas.drawBitmap(bitmap,0,0,paint);
+            canvas.restore();
+        }
+    }
+}
