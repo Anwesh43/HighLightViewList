5f1f4cce7308acb577af12574b1e29b531350ba6
Anwesh43 <anweshthecool0@gmail.com>
2017-05-11 13:56:35 +0530

    Using HighLightViewList in app

--- app/src/main/java/com/anwesome/ui/hightlightviewlistdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/hightlightviewlistdemo/MainActivity.java
@@ -1,13 +1,26 @@
 package com.anwesome.ui.hightlightviewlistdemo;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.anwesome.ui.highlistviewlist.HighlightViewList;
+import com.anwesome.ui.highlistviewlist.SelectableImageViewList;
 
+public class MainActivity extends AppCompatActivity {
+    Bitmap bitmap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(bitmap == null) {
+            bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.back3);
+        }
+        HighlightViewList highlightViewList = new HighlightViewList(this);
+        for(int i=0;i<12;i++) {
+            highlightViewList.addImage(bitmap);
+        }
+        highlightViewList.show();
     }
 }
